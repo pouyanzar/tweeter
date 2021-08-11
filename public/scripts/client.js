6348aa9c9a842713f01ad40bd144d8a72ed005bf
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-08-11 23:37:52 +0000

    client.js implemented to render tweets in index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const createTweetElement = function(tweet) {
+
+  $tweet = `<article>
+  <div class="tweet-article-header">
+   <div>
+     <img src="https://i.imgur.com/nlhLi3I.png" />
+     <span>${tweet.user.name}</span>
+   </div>
+     <span>${tweet.user.handle}</span>
+  </div>
+   <p class="tweet-article">${tweet.content.text}</p>
+  <div class="tweet-article-footer">
+   <div>
+     <p>${timeago.format(tweet.created_at)}</p>
+   </div>
+   <div class="tweet-article-icons">
+     <i class="fas fa-flag"></i>
+     <i class="fas fa-retweet"></i>
+     <i class="fas fa-heart"></i>
+   </div>
+  </div>
+ </article>`
+
+  return $tweet;
+}
+
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $(document).ready(function() {
+      $('#tweets-container').append($tweet);
+    })
+  }
+}
+
+renderTweets(data);
