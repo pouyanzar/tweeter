40f6e6c90a9fea55a390fd7763b551af478c7dee
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-08-11 21:04:07 +0000

    bug fixed and refactored

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-  $('#tweet-text').on('keypress', function(e) {
-    $('.counter')[0].defaultValue--;
-    if ($('.counter')[0].defaultValue < 0) {
-      $('.counter').css('color', '#ff0000');
+  let tweet = $(this).find('#tweet-text');
+  let counter = $(this).find('.counter')
+  $(tweet).on('input', function() {
+    let counterValue = counter.val(140 - $(this).val().length);
+    if (counterValue.val() < 0) {
+      $(counter).css('color', '#ff0000');
+    } else {
+      $(counter).css('color', '#000000');
     }
   });
 });
