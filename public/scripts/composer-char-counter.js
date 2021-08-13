ce809d4370fbd84ae1453f57de45678936e70b06
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-08-13 00:12:14 +0000

    refactored and commented

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,11 @@
+//Creates the counter to display number of remaining character in the form field
 $(document).ready(function() {
   let tweet = $(this).find('#tweet-text');
-  let counter = $(this).find('.counter')
+  let counter = $(this).find('.counter');
   $(tweet).on('input', function() {
     let counterValue = counter.val(140 - $(this).val().length);
+
+    //changes the color of counter if exceed the maximum number of character
     if (counterValue.val() < 0) {
       $(counter).css('color', '#ff0000');
     } else {
